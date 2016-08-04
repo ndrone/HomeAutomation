e9cbb7f33ce531b8dbbdd997932f202f1b05e81b
Nicholas Drone <ndrone@gmail.com>
2016-08-03 20:12:46 -0400

    Updated to use Config Server

--- GarageDoorOpener-server/src/main/java/org/dronezone/garagedoor/GarageDoorConfiguration.java
+++ GarageDoorOpener-server/src/main/java/org/dronezone/garagedoor/GarageDoorConfiguration.java
@@ -1,7 +1,9 @@
 package org.dronezone.garagedoor;
 
 import com.pi4j.io.gpio.GpioFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +17,7 @@ import org.springframework.context.annotation.Configuration;
 public class GarageDoorConfiguration
 {
     @Bean
+    @RefreshScope
     public GarageDoorService garageDoorService(GarageDoorProperties garageDoorProperties)
     {
         return new GarageDoorServiceImpl(garageDoorProperties, GpioFactory.getInstance());
