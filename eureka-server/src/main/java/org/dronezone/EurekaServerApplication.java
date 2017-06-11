882d42eaf00af3507acc63ace7a5039291bd50a2
Nicholas Drone <ndrone@gmail.com>
2017-06-11 11:28:46 -0400

    Updated Eureka Server

--- eureka-server/src/main/java/org/dronezone/EurekaServerApplication.java
+++ eureka-server/src/main/java/org/dronezone/EurekaServerApplication.java
@@ -1,7 +1,8 @@
 package org.dronezone;
 
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
@@ -11,6 +12,7 @@ public class EurekaServerApplication
 
     public static void main(String[] args)
     {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        new SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
+//        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
